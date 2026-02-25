ad4d1a0f8081433f1bd689866b7567d3c30a4dc7
iamoptimusdad11 <iamoptimusdad11@icloud.com>
2026-02-24 22:34:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ function showSection(id) {
 /* ---------- Verse of the Day ---------- */
 async function loadVerse() {
   try {
-    const res = await fetch("https://bible-api.com/psalms 23:1");
-    const data = await res.json();
-    document.getElementById("verseText").innerText =
-      data.text + " — " + data.reference;
-  } catch {
-    document.getElementById("verseText").innerText =
-      "The Lord is my shepherd; I shall not want. — Psalm 23:1";
-  }
+    const res = await fetch(
+  "https://beta.ourmanna.com/api/v1/get/?format=json",
+  { cache: "no-store" }
+);
+const data = await res.json();
+document.getElementById("verseText").innerText =
+  data.verse.details.text; }
 }
 loadVerse();
 
